8457b8d3b9c97d4630f5f2a46fb77cf128adb371
Nanamoriyama <nanamoriyama@outlook.jp>
2024-05-07 15:59:58 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,25 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
+
 app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
 app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
@@ -17,7 +29,8 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7500, () => {
-  console.log("Server started on localhost:7500");
+  console.log("server started on localhost:7500");
 });
